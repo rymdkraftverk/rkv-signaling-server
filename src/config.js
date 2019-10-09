ac09ec129ef06d37d45d4a0a9649eacfffe31693
Lorentz Lasson <lasson.lorentz@gmail.com>
2019-10-09 22:22:54 +0200

    Add default cors url

--- src/config.js
+++ src/config.js
@@ -3,7 +3,9 @@ const { env } = process
 const parseCsv = x => x.split(',')
 
 const config = {
-  corsWhitelist: env.CORS_WHITELIST ? parseCsv(env.CORS_WHITELIST) : [],
+  corsWhitelist: env.CORS_WHITELIST
+    ? parseCsv(env.CORS_WHITELIST) :
+    ['http://localhost:8081'],
 }
 
 module.exports = config
